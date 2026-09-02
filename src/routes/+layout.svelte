32c0f6e7b2f1a4587e9fc79514b436045f046377
Juan <juandiegocastellanoperez2004@gmail.com>
2026-09-02 16:55:19 -0400

    Comienzo de mi pagina web

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<nav>
+	<div>
+		<span></span>
+		<p>Vinilos Clásicos</p>
+	</div>
+
+	<div>
+		<a href="/">Inicio</a>
+		<a href="/catalogo">Catálogo</a>
+		<a href="/categorias">Categorias</a>
+		<a href="/artistas">Artistas</a>
+	</div>
+
+	<div>
+		<span></span>
+		<a href="/carrito">
+			<span></span>
+		</a>
+	</div>
+</nav>
+
 {@render children()}
+
+<footer>Este es mi pie de pagina</footer>

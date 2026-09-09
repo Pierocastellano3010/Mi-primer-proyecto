f6eda488edc26ff4b03f302195c18dba5bf9eef4
Juan <juandiegocastellanoperez2004@gmail.com>
2026-09-08 21:51:03 -0400

    Empece a añadir las categorias de mi pagina

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,4 +10,22 @@
         <button>rock clasico</button>
         <button>Rock en Españól</button>
     </div>
+    <div>
+        <h2>Ed maverick</h2>
+        <p>
+            El joven cantautor chihuahuense que redefinió la melancolía del folk
+            contemporáneo mexicano. Sus prensajes en vinilo capturanla cruda
+            intimidad de su voz
+        </p>
+        <button>Explorar colección</button>
+    </div>
+    <div>
+        <h3>Enanitos Verdes</h3>
+        <p>
+            Leyendas indiscutibles del rock en español. Redescubre los himnos de
+            toda una generacion con la calidez inigualable que solo el formato
+            analógico puede ofrecer.
+        </p>
+        <button>Explorar Colección</button>
+    </div>
 </section>
---
+++ src/routes/catalogo/+page.svelte
@@ -0,0 +1,14 @@
+<div>
+    <h1>Catálogo Sonoro</h1>
+    <p>
+        Explora nuestra cuidada selección de vinilos de primera edición. Sonido
+        analógico restaurado que rinde homenaje a las leyendas del rock y
+        folclor
+    </p>
+</div>
+<div>
+<button>Todos</button>
+<button>Folk</button>
+<button>Rock clásico</button>
+<button>Artistas Destacados</button>
+</div>
